19d45e9f0c1ccf04ce37a263e77cf0581996dd79
Колесниченко Леонид <Leon-kol1994@rambler.ru>
2022-08-11 23:17:55 +0300

    zadacha36

---
+++ Zadacha36/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+void Zadacha36()
+{
+    int size = 10;
+    int[] array = new int[size];
+
+    int sum = 0;
+    for (int i = 1; i < size; i += 2)
+    {
+        sum += array[i];
+    }
+
+    Console.WriteLine(sum);
+
+    
+    void FillArray(int[] array)
+    {
+        Random random = new Random();
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(0, 100);
+        }
+        void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+Zadacha36();
