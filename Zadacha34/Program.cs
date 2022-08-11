8f8e968b93a1d7b55ef655d985668a15a7c85ce4
Колесниченко Леонид <Leon-kol1994@rambler.ru>
2022-08-11 22:58:19 +0300

    zadacha34

---
+++ Zadacha34/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 34: Задайте массив заполненный случайнымиположительными трёхзначными числами. 
+// Напишитепрограмму, которая покажет количество чётных чисел вмассиве.
+
+void Zadacha34 ()
+{
+    int size = 10;
+    int[] array = new int[size];
+
+    FillArray(array);
+    PrintArray(array);
+    
+    int count = 0;
+    for (int i = 0; i < size; i ++)
+    {
+        if (array[i] % 2 == 0) count += 1;
+    }
+
+    Console.WriteLine(count);
+
+    void FillArray (int [] array)
+{
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(100, 1000);
+    }
+}   
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+} 
+
+}
+Zadacha34();
